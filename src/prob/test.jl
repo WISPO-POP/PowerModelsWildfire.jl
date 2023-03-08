9f6c82d80412b487ab856727a68dac6871ad5cfd
Noah Rhodes <nrhodes@wisc.edu>
2023-03-08 16:01:43 -0600

    add _run_strg_mops

--- src/prob/test.jl
+++ src/prob/test.jl
@@ -234,4 +234,143 @@ function _build_strg_ops(pm::_PM.AbstractPowerModel)
 end
 
 # MOPS with storage problem
+""
+function _run_strg_mops(file, model_constructor::Type, optimizer; kwargs...)
+    return _PM.solve_model(file, model_constructor, optimizer, _build_mn_strg_ops;
+        multinetwork=true, ref_extensions=[_PM.ref_add_on_off_va_bounds!], kwargs...)
+end
 
+
+function _build_mn_strg_ops(pm::_PM.AbstractPowerModel)
+    for (n, network) in _PM.nws(pm)
+
+        variable_bus_active_indicator(pm, nw=n)
+        variable_bus_voltage_on_off(pm, nw=n)
+
+        _PM.variable_gen_indicator(pm, nw=n)
+        _PM.variable_gen_power_on_off(pm, nw=n)
+
+        _PM.variable_storage_indicator(pm, nw=n)
+        _PM.variable_storage_power_mi_on_off(pm, nw=n)
+
+        _PM.variable_branch_indicator(pm, nw=n)
+        _PM.variable_branch_power(pm, nw=n)
+
+        _PM.variable_load_power_factor(pm, nw=n, relax=true)
+        _PM.variable_shunt_admittance_factor(pm, nw=n, relax=true)
+
+        _PM.constraint_model_voltage_on_off(pm, nw=n)
+        for i in _PM.ids(pm, :ref_buses, nw=n)
+            _PM.constraint_theta_ref(pm, i, nw=n)
+        end
+
+        for i in _PM.ids(pm, :gen, nw=n)
+            constraint_generation_active(pm, i, nw=n)
+        end
+
+        for i in _PM.ids(pm, :bus, nw=n)
+            constraint_bus_voltage_on_off(pm, i)
+            _PMR.constraint_power_balance_shed(pm, i, nw=n)
+        end
+
+        for i in _PM.ids(pm, :storage, nw=n)
+            constraint_storage_active(pm, i, nw=n)
+            _PM.constraint_storage_state(pm, i, nw=n)
+            _PM.constraint_storage_complementarity_mi(pm, i, nw=n)
+            _PM.constraint_storage_on_off(pm,i, nw=n)
+            _PM.constraint_storage_losses(pm, i, nw=n)
+            _PM.constraint_storage_thermal_limit(pm, i, nw=n)
+        end
+
+        for i in _PM.ids(pm, :branch, nw=n)
+            constraint_branch_active(pm, i, nw=n)
+            _PM.constraint_ohms_yt_from_on_off(pm, i, nw=n)
+            _PM.constraint_ohms_yt_to_on_off(pm, i, nw=n)
+
+            _PM.constraint_voltage_angle_difference_on_off(pm, i, nw=n)
+
+            _PM.constraint_thermal_limit_from_on_off(pm, i, nw=n)
+            _PM.constraint_thermal_limit_to_on_off(pm, i, nw=n)
+        end
+
+        for i in _PM.ids(pm, :load, nw=n)
+            constraint_load_active(pm, i, nw=n)
+        end
+    end
+
+    network_ids = sort(collect(_PM.nw_ids(pm)))
+
+    n_1 = network_ids[1]
+    for i in _PM.ids(pm, :storage, nw=n_1)
+        _PM.constraint_storage_state(pm, i, nw=n_1)
+    end
+
+    for n_2 in network_ids[2:end]
+        for i in _PM.ids(pm, :storage, nw=n_2)
+            _PM.constraint_storage_state(pm, i, n_1, n_2)
+        end
+        for i in _PM.ids(pm, :gen, nw=n_2)
+            constraint_gen_deenergized(pm, i, n_1, n_2)
+        end
+        for i in _PM.ids(pm, :bus, nw=n_2)
+            constraint_bus_deenergized(pm, i, n_1, n_2)
+        end
+        for i in _PM.ids(pm, :storage, nw=n_2)
+            constraint_storage_deenergized(pm, i, n_1, n_2)
+        end
+        for i in _PM.ids(pm, :branch, nw=n_2)
+            constraint_branch_deenergized(pm, i, n_1, n_2)
+        end
+        for i in _PM.ids(pm, :load, nw=n_2)
+            constraint_load_deenergized(pm, i, n_1, n_2)
+        end
+        n_1 = n_2
+    end
+
+    # Add Objective Function
+    # ----------------------
+    # Maximize power delivery while minimizing wildfire risk
+    n_1 = network_ids[1]
+    if haskey(_PM.ref(pm, n_1), :risk_weight)
+        alpha = _PM.ref(pm, n_1, :risk_weight)
+    else
+        Memento.warn(_PM._LOGGER, "network data should specify risk_weight, using 0.5 as a default")
+        alpha = 0.5
+    end
+
+    for comp_type in [:gen, :load, :bus, :branch]
+        for nwid in _PM.nw_ids(pm)
+            for (id,comp) in  _PM.ref(pm, nwid, comp_type)
+                if ~haskey(comp, "power_risk")
+                    @warn "$(comp_type) $(id) does not have a power_risk value, using 0.0 as a default"
+                    comp["power_risk"] = 0.0
+                end
+            end
+        end
+    end
+
+    load_weight = Dict(nwid =>
+        Dict(i => get(load, "weight", 1.0) for (i,load) in _PM.ref(pm, nwid, :load))
+    for nwid in _PM.nw_ids(pm))
+
+    z_demand = Dict(nwid => _PM.var(pm, nwid, :z_demand) for nwid in _PM.nw_ids(pm))
+    z_storage = Dict(nwid => _PM.var(pm, nwid, :z_storage) for nwid in _PM.nw_ids(pm))
+    z_branch = Dict(nwid => _PM.var(pm, nwid, :z_branch) for nwid in _PM.nw_ids(pm))
+    z_bus = Dict(nwid => _PM.var(pm, nwid, :z_bus) for nwid in _PM.nw_ids(pm))
+    z_gen = Dict(nwid => _PM.var(pm, nwid, :z_gen) for nwid in _PM.nw_ids(pm))
+
+    JuMP.@objective(pm.model, Max,
+        sum(
+            (1-alpha)*(
+                sum(load["pd"]*load_weight[nwid][i]*z_demand[nwid][i] for (i,load) in _PM.ref(pm, nwid, :load))
+            )
+            -alpha*(
+                sum(z_branch[nwid][i]*branch["power_risk"] for (i,branch) in _PM.ref(pm, nwid, :branch))+
+                sum(z_bus[nwid][i]*bus["power_risk"] for (i,bus) in _PM.ref(pm, nwid, :bus))+
+                sum(z_gen[nwid][i]*gen["power_risk"] for (i,gen) in _PM.ref(pm, nwid, :gen))+
+                sum(z_demand[nwid][i]*load["power_risk"] for (i,load) in _PM.ref(pm, nwid, :load))+
+                sum(z_storage[nwid][i]*storage["power_risk"] for (i,storage) in _PM.ref(pm, nwid, :storage))
+            )
+        for nwid in _PM.nw_ids(pm))
+    )
+end
